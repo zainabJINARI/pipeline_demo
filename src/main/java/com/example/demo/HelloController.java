3267761a83ab099624834ae08a32c7cd4c2249f4
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2025-03-11 00:35:37 +0000

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Jenkins CI/CD";
+        return "Hello, Jenkins CI/CD!";
     }
 }
